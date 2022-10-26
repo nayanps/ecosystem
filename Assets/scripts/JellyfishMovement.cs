e2fa9d6427bcd9caeff82c2b9c654bb01e26fa3e
nayanps <nayanpsaha@gmail.com>
2022-10-25 22:28:41 -0400

    added jellyfish movement

--- Assets/scripts/JellyfishMovement.cs
+++ Assets/scripts/JellyfishMovement.cs
@@ -5,17 +5,30 @@ using UnityEngine;
 public class JellyfishMovement : MonoBehaviour
 {
     public float speed = 5.0f;
-  
+
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
         Vector3 pos = transform.position;
-        pos.y -= speed * Time.deltaTime;
+
+        pos.y += speed * Time.deltaTime;
+
+        if (pos.y > 6)
+        {
+            pos.y = -6f;
+            pos.x += 1f;
+        }
+
+        if (pos.x > 9)
+        {
+            pos.x = -9f;
+        }
+
+        transform.position = pos;
 
     }
 }
